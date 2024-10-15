205e8780d374b30b51fc06071699d373344e0e62
Haider Aly <haideralymughal@gmail.com>
2024-10-15 22:07:47 +0500

    add redirection in cofig file

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // redirects: async () => {
+  //   return [
+  //     {
+  //       source: '/foodData',
+  //       destination: "/",
+  //       permanent: false,
+  //     }
+  //   ]
+  // }
 };
 
 export default nextConfig;
